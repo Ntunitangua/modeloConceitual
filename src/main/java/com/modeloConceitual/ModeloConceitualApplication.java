b61316d8104a318021abfd37f81d1c8f08bf3540
Ntunitangua <sckytopindi1981@gmail.com>
2021-06-03 21:08:59 +0100

    Projeto foi criado

---
+++ src/main/java/com/modeloConceitual/ModeloConceitualApplication.java
@@ -0,0 +1,13 @@
+package com.modeloConceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloConceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloConceitualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modeloConceitual/ModeloConceitualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modeloConceitual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloConceitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
